54eff9a474ab5068e190a3bed99f1a4897df15b1
jon-cron <jonathanacm95@gmail.com>
2023-03-17 11:17:06 -0600

    signup form styling

--- src/pages/signup/Signup.js
+++ src/pages/signup/Signup.js
@@ -6,38 +6,54 @@ export default function Signup() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [displayName, setDisplayName] = useState("");
+  const [passwordCheck, setPasswordCheck] = useState("");
   return (
     <div className="signup">
-      <h2>Register</h2>
-      <form className="form">
-        <label>
-          <span>Email</span>
-          <input
-            type="email"
-            onChange={(e) => setEmail(e.target.value)}
-            value={email}
-            required
-          />
-        </label>
-        <label>
-          <span>User Name</span>
-          <input
-            type="text"
-            onChange={(e) => setDisplayName(e.target.value)}
-            value={displayName}
-            required
-          />
-        </label>
-        <label>
-          <span>Password</span>
-          <input
-            type="password"
-            onChange={(e) => setPassword(e.target.value)}
-            value={password}
-            required
-          />
-        </label>
-      </form>
+      <div className="form-div">
+        <h2>Register</h2>
+        <form className="form">
+          <label>
+            <span>Email</span>
+            <input
+              type="email"
+              onChange={(e) => setEmail(e.target.value)}
+              value={email}
+              required
+            />
+          </label>
+          <label>
+            <span>User Name</span>
+            <input
+              type="text"
+              onChange={(e) => setDisplayName(e.target.value)}
+              value={displayName}
+              required
+            />
+          </label>
+          <label>
+            <span>Password</span>
+            <input
+              type="password"
+              onChange={(e) => setPassword(e.target.value)}
+              value={password}
+              required
+            />
+          </label>
+          <label>
+            <span>Verify Password</span>
+            <input
+              type="password"
+              onChange={(e) => setPasswordCheck(e.target.value)}
+              value={passwordCheck}
+              required
+            />
+          </label>
+          <div className="btn-div">
+            <button>Submit</button>
+            <button>Submit</button>
+          </div>
+        </form>
+      </div>
     </div>
   );
 }
