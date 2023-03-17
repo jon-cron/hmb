bc20803f38fdc1a771f2086e2610b65c933ab422
jon-cron <jonathanacm95@gmail.com>
2023-03-17 10:27:42 -0600

    auth pages created

--- src/App.js
+++ src/App.js
@@ -1,15 +1,38 @@
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+//styles
 import "./App.css";
+//react
+import {
+  BrowserRouter as Router,
+  Navigate,
+  Route,
+  Routes,
+} from "react-router-dom";
+//hooks
+import { useAuthContext } from "./hooks/useAuthContext.js";
 //pages
 import Home from "./pages/home/Home.js";
 function App() {
+  const { authIsReady, user } = useAuthContext();
   return (
     <div className="App">
-      <Router>
-        <Routes>
-          <Route path="/" element={<Home />} />
-        </Routes>
-      </Router>
+      {authIsReady && (
+        <Router>
+          <Routes>
+            <Route
+              path="/"
+              element={user ? <Home /> : <Navigate to="/login" />}
+            />
+            <Route
+              path="/login"
+              element={!user ? <Signup /> : <Navigate to="/" />}
+            />
+            <Route
+              path="/signup"
+              element={!user ? <Home /> : <Navigate to="/" />}
+            />
+          </Routes>
+        </Router>
+      )}
     </div>
   );
 }
