21690dc732e3446e945d0b4df36e100a0ba96667
jon-cron <jonathanacm95@gmail.com>
2023-03-18 12:37:30 -0600

    login page made

--- src/pages/login/Login.js
+++ src/pages/login/Login.js
@@ -1,7 +1,41 @@
 import "./Login.css";
 
-import React from "react";
+import { useState } from "react";
 
-export default function Login() {
-  return <div>Login</div>;
+export default function () {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  return (
+    <div className="signup">
+      <div className="form-div">
+        <h2>Register</h2>
+        <form className="form">
+          <label>
+            <span>Email:</span>
+            <input
+              type="email"
+              onChange={(e) => setEmail(e.target.value)}
+              value={email}
+              required
+            />
+          </label>
+
+          <label>
+            <span>Password:</span>
+            <input
+              type="password"
+              onChange={(e) => setPassword(e.target.value)}
+              value={password}
+              required
+            />
+          </label>
+
+          <div className="btn-div">
+            <button>Submit</button>
+            <button>Submit</button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
 }
