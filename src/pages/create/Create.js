73da6a5dde166edde6339e67e706cb8114c7365c
jon-cron <jonathanacm95@gmail.com>
2023-03-19 12:42:25 -0600

    useState in form

--- src/pages/create/Create.js
+++ src/pages/create/Create.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Create.css";
 import Select from "react-select";
 // NOTE must "npm i react-google-autocomplete --save" before using
@@ -16,25 +16,41 @@ const categories = [
   { value: "Other", label: "Other" },
 ];
 export default function Create() {
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+  const [location, setLocation] = useState("");
+  const [capacity, setCapacity] = useState("");
+  const [currentItem, setCurrentItem] = useState("");
+  const [items, setItems] = useState([]);
   return (
     <div className="container">
       <div className="create-form-div">
         <form>
           <label>
             <span>Posting Title</span>
-            <input type="text" />
+            <input onChange={(e) => setTitle(e.target.value)} type="text" />
           </label>
           <label>
             <span>Description</span>
-            <textarea rows={10}></textarea>
+            <textarea
+              onChange={(e) => setDescription(e.target.value)}
+              rows={10}
+            ></textarea>
           </label>
           <label>
             <span># of workers</span>
-            <input type="number" />
+            <input
+              onChange={(e) => setCapacity(e.target.value)}
+              type="number"
+            />
           </label>
           <label>
             <span>Tools needed</span>
-            <Select options={categories} isMulti />
+            <Select
+              options={categories}
+              isMulti
+              onChange={(option) => setCurrentItem(option)}
+            />
           </label>
           <label>
             <span>Location</span>
