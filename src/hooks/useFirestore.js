faceaddb5ff9616c65daf475550fcb88f98f7cd6
jon-cron <jonathanacm95@gmail.com>
2023-03-20 09:38:49 -0600

    first push of the day

---
+++ src/hooks/useFirestore.js
@@ -0,0 +1,4 @@
+import { db } from "../firebase/config.js";
+import { useReducer, useEffect, useState } from "react";
+import { Timestamp } from "firebase/firestore";
+export const useFirestore = () => {};
