b0b9e197dce33197827d79a1a72291ae68c69c95
jon-cron <jonathanacm95@gmail.com>
2023-03-20 09:42:37 -0600

    useFirestore structure made

--- src/hooks/useFirestore.js
+++ src/hooks/useFirestore.js
@@ -1,4 +1,20 @@
 import { db } from "../firebase/config.js";
 import { useReducer, useEffect, useState } from "react";
 import { Timestamp } from "firebase/firestore";
-export const useFirestore = () => {};
+
+let initialState = {
+  document: null,
+  isPending: false,
+  error: null,
+  success: null,
+};
+const firestoreReducer = (state, action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
+export const useFirestore = (collection) => {
+  const [response, dispatch] = useReducer(firestoreReducer, initialState);
+  const [isCancelled, setIsCancelled] = useState(false);
+};
