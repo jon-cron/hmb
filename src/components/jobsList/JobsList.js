f94303b171d3fea0cbfb9f529ddbe358711a457f
jon-cron <jonathanacm95@gmail.com>
2023-03-21 08:16:07 -0600

    rendering joblist

---
+++ src/components/jobsList/JobsList.js
@@ -0,0 +1,16 @@
+import "./JobsList.css";
+
+import React from "react";
+import { Link } from "react-router-dom";
+
+export default function JobsList({ jobs }) {
+  return (
+    <div className="jobs-list">
+      {jobs.map((j) => (
+        <Link to={`/jobs/${j.id}`} key={j.id} className="job-card">
+          {j.job.title}
+        </Link>
+      ))}
+    </div>
+  );
+}
--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -2,13 +2,14 @@ import "./Home.css";
 import React from "react";
 import { useAuthContext } from "../../hooks/useAuthContext.js";
 import { useCollection } from "../../hooks/useCollection.js";
+import JobsList from "../../components/jobsList/JobsList.js";
 export default function Home() {
   const { documents } = useCollection("jobs");
   const { user } = useAuthContext();
   console.log(documents);
   return (
     <div className="container">
-      <div></div>
+      <div>{documents && <JobsList jobs={documents} />}</div>
     </div>
   );
 }
