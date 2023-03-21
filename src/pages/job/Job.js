4eba1e1dc4ce7ee9e788cbf6a769f8f7de034544
jon-cron <jonathanacm95@gmail.com>
2023-03-21 10:58:53 -0600

    styling and making offer form

--- src/pages/job/Job.js
+++ src/pages/job/Job.js
@@ -2,7 +2,34 @@ import "./Job.css";
 import { useParams } from "react-router-dom";
 import { useDocument } from "../../hooks/useDocument.js";
 import { useAuthContext } from "../../hooks/useAuthContext.js";
+import ReactSelect from "react-select";
+import { useState } from "react";
+
+const offerAmounts = [
+  { value: 6, label: 6 },
+  { value: 12, label: 12 },
+  { value: 18, label: 18 },
+  { value: 24, label: 24 },
+  { value: 30, label: 30 },
+  { value: 48, label: 48 },
+  { value: 72, label: 72 },
+];
+const offerType = [
+  { value: "Bud light", label: "Bud light" },
+  { value: "Budweiser", label: "Budweiser" },
+  { value: "Coors", label: "Coors" },
+  { value: "Coors Light", label: "Coors Light" },
+  { value: "Corona", label: "Corona" },
+  { value: "Corona Premier", label: "Corona Premier" },
+  { value: "Michelob Ultra", label: "Michelob Ultra" },
+  { value: "Miller Light", label: "Miller Light" },
+  { value: "Natural Light", label: "Natural Light" },
+  { value: "Naturdays", label: "Naturdays" },
+  { value: "Samuel Adams", label: "Samuel Adams" },
+];
 export default function Job() {
+  const [amount, setAmount] = useState("");
+  const [type, setType] = useState("");
   // const [job, setJob] = useState(null);
   const params = useParams();
   const { user } = useAuthContext();
@@ -18,16 +45,61 @@ export default function Job() {
               <span className="job-name">
                 <h3>{job.title}</h3>
               </span>
-              <span className="job-description">
+              <span className="job-flex">
                 <h4>Description:</h4>
                 {job.description}
               </span>
+              <span className="job-flex-evenly">
+                <div>
+                  <h4>Workers Needed:</h4>
+                  {job.totalWorkers}
+                </div>
+                <div>
+                  <h4>Estimated Time:</h4>
+                  {job.hours}(hours)
+                </div>
+              </span>
+              <span className="job-flex">
+                <h4>Preferred Items:</h4>
+                <ul>
+                  {job.items.map((i) => (
+                    <li key={i.label}> {i.label} </li>
+                  ))}
+                </ul>
+              </span>
+              {job.offers.includes(user.uid) && (
+                <span className="job-flex">
+                  <h4>Location</h4>
+                  {job.location}
+                </span>
+              )}
             </div>
             <div className="offer-section">
               {job.creator.id === user.uid ? (
-                <h2>Offers</h2>
+                <div>
+                  <h2>Offers</h2>
+                </div>
               ) : (
-                <h2>Make your offer!</h2>
+                <div>
+                  <h2>Make your offer!</h2>
+                  <form className="offer-form">
+                    <label>
+                      <span>Amount</span>
+                      <ReactSelect
+                        className="react-select"
+                        options={offerAmounts}
+                      />
+                    </label>
+                    <label>
+                      <span>Type</span>
+                      <ReactSelect
+                        className="react-select"
+                        options={offerType}
+                      />
+                    </label>
+                    <button className="btn">Submit</button>
+                  </form>
+                </div>
               )}
             </div>
           </div>
