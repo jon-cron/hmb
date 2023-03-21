472188cd4bdebb499c5ef691e0a75795bd1f784c
jon-cron <jonathanacm95@gmail.com>
2023-03-21 10:19:18 -0600

    styling job page

--- src/pages/job/Job.js
+++ src/pages/job/Job.js
@@ -1,15 +1,38 @@
 import "./Job.css";
 import { useParams } from "react-router-dom";
 import { useDocument } from "../../hooks/useDocument.js";
+import { useAuthContext } from "../../hooks/useAuthContext.js";
 export default function Job() {
   // const [job, setJob] = useState(null);
   const params = useParams();
+  const { user } = useAuthContext();
   const { document: job } = useDocument("jobs", params.id);
   console.log(job);
   return (
-    <div className="job-page">
-      <h2>Job Page</h2>
-      {job && <h2>{job.title}</h2>}
+    <div className="container">
+      <div className="job-page">
+        <h2 className="job-title">Job Page</h2>
+        {job && (
+          <div className="job-section">
+            <div className="job-info">
+              <span className="job-name">
+                <h3>{job.title}</h3>
+              </span>
+              <span className="job-description">
+                <h4>Description:</h4>
+                {job.description}
+              </span>
+            </div>
+            <div className="offer-section">
+              {job.creator.id === user.uid ? (
+                <h2>Offers</h2>
+              ) : (
+                <h2>Make your offer!</h2>
+              )}
+            </div>
+          </div>
+        )}
+      </div>
     </div>
   );
 }
