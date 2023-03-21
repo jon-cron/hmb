cbfb92dc6389df0543c759c84935634b11a8523d
jon-cron <jonathanacm95@gmail.com>
2023-03-21 13:16:50 -0600

    firebase 9 snapshot note

--- src/hooks/useDocument.js
+++ src/hooks/useDocument.js
@@ -6,6 +6,7 @@ export const useDocument = (c, id) => {
   const [document, setDocument] = useState(null);
 
   useEffect(() => {
+    // NOTE here is a good example of how to get a snapshot of a single doc using Firebase 9
     const unsub = async () => {
       const jobRef = doc(db, c, id);
       const unsub = onSnapshot(jobRef, (snapshot) => {
